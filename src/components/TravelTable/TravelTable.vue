a0144bab43302fa464355c538f0b08046d3dc5f7
Bernardo Simoes <bersimoes@gmail.com>
2019-07-06 11:36:51 +0100

    Add TravelTable component

---
+++ src/components/TravelTable/TravelTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Header content 1</th>
+        <th>Header content 2</th>
+      </tr>
+    </thead>
+    <tfoot>
+      <tr>
+        <td>Footer content 1</td>
+        <td>Footer content 2</td>
+      </tr>
+    </tfoot>
+    <tbody>
+      <tr>
+        <td>Body content 1</td>
+        <td>Body content 2</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class TravelTable extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>Despesas de Deslocação</h1>
+
+    <travel-table></travel-table>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import TravelTable from "@/components/TravelTable/TravelTable.vue";
+
+@Component({
+  components: {
+    TravelTable
+  }
+})
+export default class Home extends Vue {}
+</script>
