ab50c5b2aa6840b8dc5c965fd94f0b8f192ffaef
Bernardo Simoes <bersimoes@gmail.com>
2019-07-10 14:39:30 +0100

    Add time input to arrival and departure times

--- src/components/TravelTable/TravelRow.vue
+++ src/components/TravelTable/TravelRow.vue
@@ -1,9 +1,13 @@
 <template>
   <tr>
-    <td>{{ day.getUTCDate() }}</td>
+    <td>{{ day.getUTCDate() }} | {{ weekDay(day) }}</td>
     <td>{{ description }}</td>
-    <td>{{ departure.getHours() }}</td>
-    <td>{{ arrival.getHours() }}</td>
+    <td>
+      <input type="time" :max="hours(arrival)" :value="hours(departure)" />
+    </td>
+    <td>
+      <input type="time" :min="hours(departure)" :value="hours(arrival)" />
+    </td>
     <td>34,60</td>
   </tr>
 </template>
@@ -11,12 +15,21 @@
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
 import { Moment } from "moment";
+import moment from "moment";
 
 @Component
 export default class TravelRow extends Vue {
   @Prop() public day!: Date;
-  @Prop() public departure!: Date;
   @Prop() public description!: Date;
   @Prop() public arrival!: Date;
+  @Prop() public departure!: Date;
+
+  hours(date: Date) {
+    return moment(date).format("HH:mm");
+  }
+
+  weekDay(date: Date) {
+    return moment(date).format("ddd");
+  }
 }
 </script>
