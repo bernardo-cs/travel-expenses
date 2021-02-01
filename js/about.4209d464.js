d4d4428b3a614bc4083291d8e9fb06f14faba844
bernardo <bersimoes@gmail.com>
2021-02-01 12:02:11 +0000

    deploy

---
+++ js/about.4209d464.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var s=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[e("h1",[t._v("\n    "+t._s(t.$t("about.description"))+"\n  ")])])},a=[],u=e("2877"),o={},c=Object(u["a"])(o,s,a,!1,null,null,null);n["default"]=c.exports}}]);
+//# sourceMappingURL=about.4209d464.js.map
---
+++ precache-manifest.9d49e6ce932c8dcef2e5f539c0ecfdbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f126d916840b957c5e9",
+    "url": "/travel-expenses/js/about.4209d464.js"
+  },
+  {
+    "revision": "a8be03f871f07156a6fc",
+    "url": "/travel-expenses/css/app.dd1b9eba.css"
+  },
+  {
+    "revision": "a8be03f871f07156a6fc",
+    "url": "/travel-expenses/js/app.9fdeeef6.js"
+  },
+  {
+    "revision": "e14e5338af0bb3e5ca25",
+    "url": "/travel-expenses/js/chunk-1448492b.752a6a95.js"
+  },
+  {
+    "revision": "3b8f749f73ef546211cd",
+    "url": "/travel-expenses/js/chunk-81969e14.153ba93b.js"
+  },
+  {
+    "revision": "f3a1c58eba85a636b4c9",
+    "url": "/travel-expenses/css/chunk-vendors.5557fef5.css"
+  },
+  {
+    "revision": "f3a1c58eba85a636b4c9",
+    "url": "/travel-expenses/js/chunk-vendors.1fab9435.js"
+  },
+  {
+    "revision": "38f2d54173145d7648ef6eac0a2eecdd",
+    "url": "/travel-expenses/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/travel-expenses/robots.txt"
+  }
+];
